d4b24976fb62b33ef7b1ac836aa6442360a41af2
Morten Gjermansen <mgjm@novozymes.com>
2021-11-11 23:36:29 +0100

    progress

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datasaurus",
+    version="0.1",
+    description="Python wrapper for The Datasaurus Dozen dataset",
+    author="Morten Gjermansen",
+    url="https://github.com/gjerman/datasaurus",
+    license="MIT License",
+    py_modules=["datasaurus"],
+)
